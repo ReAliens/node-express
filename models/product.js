b3ac98e3ad03f805e75df3c05ed6e1c686eb3971
a.rida <ahmedreda152@gmail.com>
2024-06-03 02:15:33 +0300

    enhance app views and add another routes

--- models/product.js
+++ models/product.js
@@ -2,22 +2,27 @@ const fs = require("fs");
 const path = require("path");
 
 const p = path.join(
-  path.dirname(require.main.filename),
+  path.dirname(process.mainModule.filename),
   "data",
   "products.json"
 );
+
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
