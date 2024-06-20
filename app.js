5ddaf8e86ca70132e76b8d4b77e811ac72f3144e
a.rida <ahmedreda152@gmail.com>
2024-06-20 19:09:13 +0300

    finish auth with flash messages

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoDBStrore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
+
 const MONGODB_URI =
   "mongodb+srv://ahmedreda152:EQcK4nnEuAfnUqJ1@cluster0.y52xdx6.mongodb.net/shop?w=majority&appName=Cluster0";
 
 const app = express();
-const store = new MongoDBStrore({
+const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -31,20 +35,28 @@ app.use(
     secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store,
+    store: store,
   })
 );
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
-  User.findById(req.session.user)
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
     })
     .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use("/admin", adminRoutes);
@@ -62,4 +74,4 @@ mongoose
     console.log(err);
   });
 
-//    "mongodb+srv://ahmedreda152:EQcK4nnEuAfnUqJ1@cluster0.y52xdx6.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+// "mongodb+srv://ahmedreda152:EQcK4nnEuAfnUqJ1@cluster0.y52xdx6.mongodb.net/shop?w=majority&appName=Cluster0";
